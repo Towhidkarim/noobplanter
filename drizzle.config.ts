dbf98831b21b0568b7db359573914f851c563111
Towhidkarim <towhidkarim123@gmail.com>
2024-03-11 01:51:55 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   drizzle.config.ts
    	modified:   lib/db/migrate.ts
    	modified:   package.json
    	modified:   pnpm-lock.yaml

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,8 @@ export default {
   out: './lib/db',
   driver: 'pg',
   dbCredentials: {
-    connectionString: 'postgres://postgres:414335@localhost:5432/postgres',
+    connectionString:
+      'postgresql://towhidkarim123:ukbX1ZY2deTj@ep-soft-dream-a13oewxm.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
   },
   verbose: true,
   strict: true,
--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,6 +1,7 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { Client, Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from '@neondatabase/serverless';
+import type {} from 'drizzle-orm/neon-http';
+// import { Client, Pool } from 'pg';
 import * as schema from '././schema';
 
 // const migrationClient = postgres(process.env.DB_URI!, { max: 1 });
@@ -12,23 +13,25 @@ const client = new Pool({ connectionString: process.env.DB_URI!, max: 2 });
 async function dbconnect() {
   await client.connect();
 }
-dbconnect();
+// dbconnect();
+const sql = new Pool({ connectionString: process.env.NEON_URI! });
+const db = drizzle(sql);
 
 // export const db = drizzle(client, { schema });
 // export default db;
-declare global {
-  // eslint-disable-next-line no-var -- only var works here
-  var db: NodePgDatabase<typeof schema> | undefined;
-}
+// declare global {
+//   // eslint-disable-next-line no-var -- only var works here
+//   var db: NodePgDatabase<typeof schema> | undefined;
+// }
 
-let db: NodePgDatabase<typeof schema>;
+// let db: NodePgDatabase<typeof schema>;
 
-if (process.env.NODE_ENV === 'production') {
-  db = drizzle(client, { schema });
-} else {
-  if (!global.db) global.db = drizzle(client, { schema });
+// if (process.env.NODE_ENV === 'production') {
+//   db = drizzle(client, { schema });
+// } else {
+//   if (!global.db) global.db = drizzle(client, { schema });
 
-  db = global.db;
-}
+//   db = global.db;
+// }
 
 export default db;
